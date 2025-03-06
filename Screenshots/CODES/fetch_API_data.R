c4f3d078f78d98d3294ef14d7e333368038d6a98
SHASHANK KUMAR LAL <87319766+shashankkumarlal@users.noreply.github.com>
2025-03-06 16:10:56 +0530

    Update fetch_API_data.R

--- Screenshots/CODES/fetch_API_data.R
+++ Screenshots/CODES/fetch_API_data.R
@@ -2,10 +2,13 @@ library(httr)
 library(jsonlite)
 
 # Define API URL
-api_url <- "https://abcd1234.execute-api.us-east-1.amazonaws.com/weather"
+api_url <- "https://f2r1kaj839.execute-api.us-east-1.amazonaws.com/prod/weather"
 
-# Fetch Data
+# Fetch data from API
 response <- GET(api_url)
-data <- fromJSON(content(response, "text"))
 
-print(data)  # Check the data
+# Convert JSON response to R list
+weather_data <- fromJSON(content(response, "text"))
+
+# Print the weather data
+print(weather_data)
